ea1e90ae6a3bc2f4b3afc0d1f75aeba35d8734ec
Botir Tursunov <xasanovich01@icloud.com>
2023-02-12 16:31:59 +0500

    new

--- softit/settings/prod.py
+++ softit/settings/prod.py
@@ -1,7 +1,7 @@
 from decouple import config
 
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['192.168.10.8', 'localhost', '127.0.0.1', 'api.softit.sport.uz']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
